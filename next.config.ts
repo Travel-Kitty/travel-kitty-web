c6f1ca7bda4e011e094b7066275fe4ae3ee81a0c
dhisa <ijlalwindhi15@gmail.com>
2025-10-14 15:40:45 +0700

    fix(next.config): ensure images domain configuration is correctly set

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: { serverActions: { allowedOrigins: ["*"] } },
+  images: { domains: ["avatar.vercel.sh"] },
 };
 
 export default nextConfig;
