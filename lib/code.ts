cc0f9eab5944c1cd44ea7fe47cab8f78a268334b
dhisa <ijlalwindhi15@gmail.com>
2025-10-24 01:40:32 +0700

    refactor(trip): remove CreateTripDialog, JoinByCode, TripsList components and makeCode6 utility function

--- lib/code.ts
+++
@@ -1,7 +0,0 @@
-export function makeCode6() {
-  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"; // avoid 0/O/1/I
-  let s = "";
-  for (let i = 0; i < 6; i++)
-    s += alphabet[Math.floor(Math.random() * alphabet.length)];
-  return s;
-}
